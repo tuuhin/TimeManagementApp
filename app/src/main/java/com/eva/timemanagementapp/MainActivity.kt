f2901f84181042c582709d65fcff2e89ffbc929c
tuuhin <tuhinbhowmick2513@gmail.com>
2023-12-25 20:38:20 +0530

    Fixed splash screen problem for the app There was some problem with the splash screen sometime it stayed and no action can be preformed as the action is determined by a flow a collected will fit the job fine.

--- app/src/main/java/com/eva/timemanagementapp/MainActivity.kt
+++ app/src/main/java/com/eva/timemanagementapp/MainActivity.kt
@@ -9,13 +9,14 @@ import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -23,7 +24,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.eva.timemanagementapp.data.services.SessionService
 import com.eva.timemanagementapp.presentation.composables.NoPermissionsFound
 import com.eva.timemanagementapp.presentation.navigation.AppNavigationGraph
@@ -32,7 +36,10 @@ import com.eva.timemanagementapp.presentation.utils.checkNotificationPermissions
 import com.eva.timemanagementapp.ui.theme.TimeManagementAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -64,7 +71,6 @@ class MainActivity : ComponentActivity() {
 	override fun onStop() {
 		super.onStop()
 		unbindService(connection)
-		hasServiceBounded.update { false }
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,7 +78,15 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 
 		WindowCompat.setDecorFitsSystemWindows(window, false)
-		splash.setKeepOnScreenCondition { hasServiceBounded.value }
+
+		lifecycleScope.launch {
+			// Show the splash screen until the service is bound
+			// When bound set keepOnScreenCondition to false
+			hasServiceBounded
+				.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+				.onEach { isDone -> splash.setKeepOnScreenCondition { !isDone } }
+				.launchIn(this)
+		}
 
 		setContent {
 			TimeManagementAppTheme {
@@ -92,11 +106,11 @@ class MainActivity : ComponentActivity() {
 							AnimatedVisibility(
 								visible = isServiceBounded,
 								modifier = Modifier.fillMaxSize(),
-								enter = fadeIn(),
-								exit = fadeOut()
+								enter = fadeIn(tween(durationMillis = 600))
 							) {
 								AppNavigationGraph(service = timerService)
 							}
+							if (!isServiceBounded) Text(text = "CHECK")
 						}
 
 						else -> {
